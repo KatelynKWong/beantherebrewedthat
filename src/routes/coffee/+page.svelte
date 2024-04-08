fda8ceda51baca912714a38094cb83b57c78648d
KatelynKWong <kkw004@ucsd.edu>
2024-04-09 01:03:18 +0900

    attempts to build

--- src/routes/coffee/+page.svelte
+++ src/routes/coffee/+page.svelte
@@ -1,6 +1,10 @@
 <script>
     import CoffeeMain from "../../components/Coffee/CoffeeMain.svelte";
-  
+    // This can be false if you're using a fallback (i.e. SPA mode)
+    export const prerender = true;
+
+
+
   </script>
   
   <main>
