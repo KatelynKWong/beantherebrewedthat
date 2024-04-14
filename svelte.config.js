24f4bd1293721da3f04b8a41f2af65808a61f648
KatelynKWong <kkw004@ucsd.edu>
2024-04-14 22:47:20 +0900

    fix build

--- svelte.config.js
+++ svelte.config.js
@@ -16,8 +16,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			// base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-			base: '',
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		},
 		// routes: [
         //     // { src: 'coffee', dest: './src/routes/coffee/+page.svelte' },
