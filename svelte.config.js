900b9fa91d8c9e775a9226e66861b7a868c9bb9a
KatelynKWong <kkw004@ucsd.edu>
2024-08-20 23:27:24 -0700

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -16,7 +16,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: '/beantherebrewedthat'
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		},
 		// routes: [
         //     // { src: 'coffee', dest: './src/routes/coffee/+page.svelte' },
