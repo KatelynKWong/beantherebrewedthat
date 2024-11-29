1ecbd4d1a6694ee2f8be5f0cf3383f48924f2f49
KatelynKWong <kkw004@ucsd.edu>
2024-11-28 22:54:58 -0800

    Update CoffeeMaking.svelte
    Added entries to coffee making journal

--- src/components/Coffee/CoffeeMaking.svelte
+++ src/components/Coffee/CoffeeMaking.svelte
@@ -71,50 +71,97 @@
     let images = [
         { name: 'Inheriting Hopey\'s Espresso Machine', src: NewMachine_machine, date: '2024-03-08', 
                             gallery: [NewMachine_machine, NewMachine_latte, NewMachine_shot, NewMachine_beans], 
-                            review:'The latte has a slightly diluted flavor with a bit too much milk, making it mild and not overly bitter or sour. However, the latte art is beautifully done, adding to the overall experience. The coffee is made with beans from Honduras, which give it a smooth and balanced taste.'},
+                            review:'My close friend Hopey from church and I often visit coffee shops together. \
+                            Since she knows how much I love coffee, she gifted me her used espresso machine and \
+                            coffee grinder because she plans to upgrade to a better machine. The timing is perfect \
+                            because I had recently been researching espresso making and was considering buying an \
+                            espresso machine. However, the machine is pretty old, and it seems like the milk steam \
+                            wand is a bit broken. I may also need to buy additional tools for latte making, such as \
+                            a WDT tool, an espresso puck, and coffee beans.'},
         { name: 'Double Shot Affogato Attempt', src: Affogato, date: '2024-03-09', 
                             gallery: [Affogato, Affogato_bed, Affogato_shot],
-                            review: 'Coffee is slightly strong and could use more milk.'
+                            review: 'My roommate requested that I make her an affogato dessert at night using espresso \
+                            shots pulled from my espresso machine. So, I pulled a double shot and poured it over a \
+                            bowl of ice cream. I didn\'t realize making an affogato could be so simple.'
         },
         { name: 'First Bag of Coffee Beans: Starbucks Blonde', src: StarbucksBeans, date: '2024-03-12',
                             gallery: [StarbucksBeans, StarbucksBeans_latte, StarbucksBeans_shot1, StarbucksBeans_shot2],
-                            review: 'The latte is very sweet, with the milk overpowering the coffee’s flavor. However, the cute design adds a nice touch.'
+                            review: 'Hopey gave me a small bag of coffee beans to start practicing making espresso shots, \
+                            but it wasn\'t enough to practice much. So, I asked a friend who works at Starbucks if I could \
+                            buy a bag of beans using his employee discount. I ended up buying a bag of blonde beans for $12. \
+                            I chose the blonde beans because I think, for now, I prefer lighter roasts of coffee.'
         },
         { name: 'Converting from Cold to Hot Lattes', src: HotLatte1, date: '2024-10-17',
                             gallery: [HotLatte1, HotLatte2],
-                            review: 'The latte has the perfect amount of milk, striking a nice balance between smoothness and flavor. It’s slightly bitter without being sour, making it a satisfying cup. Medium dark roast.'
+                            review: 'This whole time, I thought my steam wand was broken. It turns out the tip was just \
+                            misaligned. However, it still doesn\'t angle properly for spinning the milk in the cup, so I \
+                            don\'t think I\'ll be able to steam milk effectively. I can, however, still use it to heat up \
+                            the milk.'
         },
         { name: 'Creating my own Pumpkin Spice Latte Recipe', src: PumpkinLatte, date: '2024-10-22',
                             gallery: [PumpkinLatte, PumpkinLatte_top, PumpkinLatte_plain, PumpkinLatte_plaintop],
-                            review: 'The Ethiopian drip was amazing with a rich and slightly bitter profile. It had just the right touch of citrus, adding a refreshing layer to the flavor. A great cup for those who enjoy a bright yet balanced coffee.'
+                            review: 'With Halloween coming up, I wanted to get into the festive spirit of the season by \
+                            making pumpkin spice lattes. Over the past week, I’ve tried pumpkin spice lattes from three \
+                            different cafes and wanted to recreate the flavors at home—reducing the sugar and intensifying \
+                            the pumpkin spice flavor. Overall, it turned out pretty well, though I could definitely use more \
+                            cinnamon next time.'
         },
         { name: 'Hot Latte with Side of Cinnamon Milk Bread', src: MilkBread_latte, date: '2024-10-26',
                             gallery: [MilkBread_latte, MilkBread_shot, MilkBread, MilkBread_set],
-                            review: 'The latte came with a complimentary small pastry, which was a delightful bonus. The latte itself featured beautiful art and had a stronger coffee flavor. It was slightly sour and less bitter than usual, but still enjoyable. The staff were incredibly friendly, making the experience even more pleasant.'
+                            review: 'Today, I had a random craving to bake milk bread. So, in the evening, I made Japanese \
+                            Hokkaido milk bread with cinnamon sugar rolled into it. The bread turned out amazing, and dipping \
+                            it into a hot latte was the perfect combination.'
         },
         { name: '3D Printing a new Tamper and Funnel', src: Tamper, date: '2024-10-28',
                             gallery: [Tamper, Funnel, ThreeD_latte, Tamper1, Tamper2],
-                            review: 'The cafe, set up in a trailer, served a drip coffee latte that was quite strong in flavor. It had the boldness of a coffee au lait but was not as rich as a traditional latte. The taste leaned more towards sour rather than bitter. Overall, a unique and flavorful experience in a cozy setting.'
+                            review: 'The tamper and funnel that came with the machine Hopey gave me didn’t fit the 51mm \
+                            portafilter properly. This often led to uneven distribution of coffee grounds in the filter and \
+                            an inconsistent tamping process, ultimately resulting in poor espresso shots. However, I recently \
+                            discovered that students can use the makerspace at UCSD for free, so I 3D-printed these two parts \
+                            and tried them out for the first time while making lattes. The pucks turned out perfectly, and I \
+                            think it will now be easier to consistently prepare good pucks.'
         },
         { name: '50% Off Aspen Beans from Three Legs Roastery', src: Aspen_latte, date: '2024-10-29',
                             gallery: [Aspen_latte, Aspen_front, Aspen_back, Aspen_latte1],
-                            review: 'The latte was slightly sweetened, but the milk was very thin, making the coffee taste more watery. However, the black sesame pudding was amazing and a highlight of the visit.'
+                            review: 'I finally used up the old beans I got from Starbucks before I left for Japan. Those beans \
+                            made it really difficult to achieve a good coffee extraction. Most of the time, there was very little \
+                            crema, and the espressos turned out extremely acidic. However, with these new beans, my first \
+                            extraction produced a generous amount of crema. The coffee was also well-balanced, with just the \
+                            right amount of acidity, a nutty, chocolatey body, and it tasted amazing as a latte.'
         },
         { name: 'Pumpkin Party: Breadge x Swag x Man', src: OtherPumpkin_latte1, date: '2024-11-02',
                             gallery: [OtherPumpkin_latte1, OtherPumpkin_latte2, OtherPumpkin_latte3, OtherPumpkin_shot],
-                            review: 'The coffee had a creamy medium flavor, thanks to the thicker cream, but the serving size was disappointingly small.'
+                            review: 'I wanted to recreate the pumpkin spice latte I made for a couple of friends, but this time \
+                            with more cinnamon and pumpkin puree, while adjusting the sugar to their taste. They all really \
+                            enjoyed it, and the espresso shots turned out consistently well. We all agreed it was very smooth, \
+                            with the perfect balance of sweetness, roasted bean bitterness, and milk.'
         },
         { name: 'Full Week of Latte Art Experimentation', src: Latte_art1, date: '2024-11-15',
                             gallery: [Latte_art1, Latte_art2, Latte_art3, Latte_art4, Latte_art5],
-                            review: 'The latte was slightly sweetened, but the milk was very thin, making the coffee taste more watery. However, the black sesame pudding was amazing and a highlight of the visit.'
+                            review: 'This week, I focused on making the best of my janky steam wand and attempting to create \
+                            some form of latte art to make my lattes more appetizing and visually appealing. I saved a few \
+                            drops of espresso, and after mixing most of the milk with the espresso shot, I poured the milk \
+                            foam on top, followed by the leftover espresso drops. Then, using a spoon or toothpick, I swirled \
+                            the espresso drops to create a simple yet tasteful design. However, this is still very much a work \
+                            in progress.'
         },
         { name: 'Branching out to Mochas', src: MochaSwirl, date: '2024-11-19',
                             gallery: [MochaSwirl, MochaSwirl_Mug, MochaSwirl_MugTop, Mocha_shot, Mocha_shotCup],
-                            review: 'The coffee had a creamy medium flavor, thanks to the thicker cream, but the serving size was disappointingly small.'
+                            review: 'My housemate brought home some Starbucks chocolate powder, perfect for making hot chocolate \
+                            or mocha syrup. With the rainy weather, I decided to make mochas for everyone. The first attempt was \
+                            too sweet, likely because Starbucks\' mocha ratios suit their sharper, more bitter coffee beans. \
+                            For the second and third attempts, I reduced the milk and chocolate syrup to highlight the coffee \
+                            flavor. The espresso shots were smooth and balanced, and overall, we all enjoyed this new drink.'
         },
         { name: 'Opening New Bag of Three Legs Roastery\'s Pink Cloud Espresso Beans', src: PinkCloud_latte, date: '2024-11-28',
                             gallery: [PinkCloud_shot, PinkCloud_latte, PinkCloud_latteTop, PinkCloud_mushroom, PinkCloud_beans],
-                            review: 'The cafe, set up in a trailer, served a drip coffee latte that was quite strong in flavor. It had the boldness of a coffee au lait but was not as rich as a traditional latte. The taste leaned more towards sour rather than bitter. Overall, a unique and flavorful experience in a cozy setting.'
+                            review: 'Over the past couple of weeks, I quickly finished the Aspen beans, so I returned to Three \
+                            Leg’s Roastery to buy more. This time, the owner recommended beans for espresso lattes and even let \
+                            me sample a few by pulling free shots. She also accelerated the roasting schedule so I could get the \
+                            Pink Cloud beans that same day, even though they were originally planned for the next day. The \
+                            service was fantastic. As seen in the photos, the crema from these beans was the thickest I’ve \
+                            ever had, and the espresso was incredibly smooth and rich. Compared to the Aspen beans’ chocolatey \
+                            notes, the Pink Cloud beans are sweeter and fruitier, making them a perfect match for espresso lattes.'
         }
     ]
 
